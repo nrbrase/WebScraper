38a545efccf6a85a5ccaefa4aa09c85366af3259
kimdobert <31771956+kimdobert@users.noreply.github.com>
2017-11-04 19:54:18 -0400

    made things faster

--- readAllLinks.java
+++ readAllLinks.java
@@ -3,9 +3,6 @@ package webby;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
 import javax.swing.JFrame;
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
@@ -15,38 +12,29 @@ import org.jsoup.nodes.Element;
 public class readAllLinks{ 
 
 	static JFrame _window;
-	public static HashMap<Integer, String> allInfo = new HashMap<Integer,String>();
-	public static Set<String> eachURL = new HashSet<String>();
 	public static String rootsite;
 	public static PrintWriter outStream;
-	public static int _key;
+	
 	
 	public static void main(String[] args) throws IOException {
 			GUI gui = new GUI();
-			_key = 0;
 		}
 	
-	protected void get_links(String url) {
-		try {
-			Document doc = Jsoup.connect(url).get();
-			Elements links = doc.select("a");
-			links.stream().map((link) -> link.attr("abs:href")).forEachOrdered((this_url) -> {
-				boolean add = eachURL.add(this_url);
-				if (add && this_url.contains(rootsite)) {
-						try {
-							getImportantInfo(this_url);
-						} catch (Exception e) {
-							// TODO Auto-generated catch block
-							e.printStackTrace();
-						}
-					get_links(this_url);
-				}
-			});
-
-		} catch (IOException ex) {
-
-		}
-	}
+	
+	public static ArrayList<String> getSites(String site) throws IOException {
+    	Document doc = null;
+    	ArrayList<String> linkCon = new ArrayList<String>();		//Creates a list to store Links
+    		doc = Jsoup.connect(site).get();
+    		Elements links = doc.select("a[href]");
+    		for (Element link : links) {
+    			String a = link.attr("href");
+    	        linkCon.add("https://www.cvs.com"+a);
+    	      }
+    	
+    	return linkCon;
+    }
+	
+	
 	protected ArrayList<String> getStates(String url) throws IOException{
 		Document doc = Jsoup.connect(url).get();
 		Elements links = doc.select("a[href]");		//Selects "href" from html code on site
@@ -60,26 +48,8 @@ public class readAllLinks{
 		}
 		return linkCon;
 	}
-	private void getImportantInfo(String url) throws IOException{
-		Document document = Jsoup.connect(url).get();
-		Elements address = document.getElementsByClass("store-address");
-		String addString = address.text();
-		Elements phoneNumber = document.getElementsByClass("phone-number");
-		String phoneString = phoneNumber.text();
-		if(allInfo.isEmpty()){
-			allInfo.put(_key, addString);
-			allInfo.put(_key+1, phoneString);
-		}
-		else if(allInfo.get(_key).equals(addString)){
-			allInfo.put(_key, addString);
-			allInfo.put(_key+1, phoneString);
-		}
-		else{
-			allInfo.put(_key+2, addString);
-			allInfo.put(_key+3, phoneString);
-			_key = _key+2;
-		}
-	}
+	
+
 }
 
 
