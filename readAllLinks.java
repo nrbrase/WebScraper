147959ecc7fc88cd2adcef542c1f0aff31a80169
Nick Brase <nrbrase@buffalo.edu>
2017-11-05 12:37:30 -0500

    Running in parallel, created all states option, deleted duplicate code

--- readAllLinks.java
+++ readAllLinks.java
@@ -9,47 +9,49 @@ import org.jsoup.nodes.Document;
 import org.jsoup.select.Elements;
 import org.jsoup.nodes.Element;
 
-public class readAllLinks{ 
-
-	static JFrame _window;
-	public static String rootsite;
-	public static PrintWriter outStream;
-	
-	
-	public static void main(String[] args) throws IOException {
-			GUI gui = new GUI();
-		}
-	
-	
-	public static ArrayList<String> getSites(String site) throws IOException {
-    	Document doc = null;
-    	ArrayList<String> linkCon = new ArrayList<String>();		//Creates a list to store Links
-    		doc = Jsoup.connect(site).get();
-    		Elements links = doc.select("a[href]");
-    		for (Element link : links) {
-    			String a = link.attr("href");
-    	        linkCon.add("https://www.cvs.com"+a);
-    	      }
-    	
-    	return linkCon;
+public class readAllLinks{
+    
+    static JFrame _window;
+    public static String rootsite;
+    public static PrintWriter outStream;
+    
+    
+    public static void main(String[] args) throws IOException {
+        GUI gui = new GUI();
     }
-	
-	
-	protected ArrayList<String> getStates(String url) throws IOException{
-		Document doc = Jsoup.connect(url).get();
-		Elements links = doc.select("a[href]");		//Selects "href" from html code on site
-		ArrayList<String> linkCon = new ArrayList<String>(); //Container of links for dropdown menu
-		for(Element link: links){
-			String a = link.attr("href");		
-			if(a.contains("locations"))			//finds everylink with a location 
-			{
-				linkCon.add(a.substring(38));	//cuts out everything besides state name
-			}
-		}
-		return linkCon;
-	}
-	
-
+    
+    //Returns /store-locator/cvs-pharmacy-locations/Alabama/CITY for all
+    public static ArrayList<String> getCities(String site) throws IOException {
+        ArrayList<String> linkCon = new ArrayList<String>();		//Creates a list to store Links
+        Document doc = Jsoup.connect(site).get();
+        Elements links = doc.select("a[href]");
+        for (Element link : links) {
+            String a = link.attr("href");
+            if(a.contains("locations/")){
+                linkCon.add(a);
+            }
+        }
+        
+        return linkCon;
+    }
+    
+    //When sent URL returns substring of state name
+    protected ArrayList<String> getStates(String url) throws IOException{
+        ArrayList<String> linkCon = new ArrayList<String>(); //Container of links for dropdown menu
+        Document doc = Jsoup.connect(url).get();
+        Elements links = doc.select("a[href]");		//Selects "href" from html code on site
+        for(Element link: links){
+            String a = link.attr("href");
+            if(a.contains("locations"))			//finds everylink with a location
+            {
+                linkCon.add(a.substring(38));	//cuts out everything besides state name
+            }
+        }
+        linkCon.add("All");
+        return linkCon;
+    }
+    
+    
 }
 
 
